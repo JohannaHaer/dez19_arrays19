7e02e20265373d8976fd89815a1af57abf00b27d
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-19 14:32:34 +0100

    Aufgabe Itteration 1_1

---
+++ assets/js/main_arrays_itteration_1_1.js.js
@@ -0,0 +1,8 @@
+let myDrinks = ["Wasser", "Schnaps", "Cocktails", "Tee"]
+
+let sortDrinks = myDrinks.sort()
+
+console.log(myDrinks);
+sortDrinks.forEach((drinks) => {
+document.write(drinks + " ")
+})
