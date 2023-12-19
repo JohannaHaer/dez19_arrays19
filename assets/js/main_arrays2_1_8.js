52aaa3e3c8fe251d1885b4924010437c3c2b01b9
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-19 13:43:34 +0100

    Aufgabe 1.8

---
+++ assets/js/main_arrays2_1_8.js
@@ -0,0 +1,6 @@
+let lieblingsreiseziele = ["Italien", "Griechenland", "Deutschland", "Indien", "Spanien"]
+
+let reiseziel2024 = lieblingsreiseziele.slice(1, 3)
+console.log(lieblingsreiseziele);
+console.log(reiseziel2024);
+console.log(lieblingsreiseziele);
